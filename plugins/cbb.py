572857b71fb9420782bda6858c68f350e11a5210
sahil-talsaniya <147942096+sahil-talsaniya@users.noreply.github.com>
2023-10-19 16:07:44 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href=https://t.me/The_Happy_Hour_Hindi>The Happy Hour</a>",
+            text = f"<b>○ Creator : <a href=tg://openmessage?user_id=6018746976>Click Here</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
